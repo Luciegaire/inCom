f0a9f095b16e1fe6f67ebf3cfdd251e8652ddb1d
Luciegaire <lucie83140@hotmail.fr>
2021-01-11 16:59:17 +0100

    creation pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { PostComponent } from './components/Feed/post/post.component';
+import { CompaniesListComponent } from './components/companies-list/companies-list.component';
+import { GenerateCvComponent } from './components/generate-cv/generate-cv.component';
+import { OffersListComponent } from './components/offers-list/offers-list.component';
+import { ProfileCompanyComponent } from './components/Profile/profile-company/profile-company.component';
+import { ProfileCandidateComponent } from './components/Profile/profile-candidate/profile-candidate.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    FeedComponent,
+    PostComponent,
+    CompaniesListComponent,
+    GenerateCvComponent,
+    OffersListComponent,
+    ProfileCompanyComponent,
+    ProfileCandidateComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
