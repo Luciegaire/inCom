fbe308e48421ab10b2488ba53238d78b4e118c4a
Pierre LE NY <pierre@volandia.fr>
2021-01-12 09:28:09 +0100

    changes names and rights

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { FeedComponent } from './components/feed/feed.component';
-import { PostComponent } from './components/Feed/post/post.component';
+import { PostComponent } from './components/feed/post/post.component';
 import { CompaniesListComponent } from './components/companies-list/companies-list.component';
 import { GenerateCvComponent } from './components/generate-cv/generate-cv.component';
 import { OffersListComponent } from './components/offers-list/offers-list.component';
-import { ProfileCompanyComponent } from './components/Profile/profile-company/profile-company.component';
-import { ProfileCandidateComponent } from './components/Profile/profile-candidate/profile-candidate.component';
+import { ProfileCompanyComponent } from './components/profile/profile-company/profile-company.component';
+import { ProfileCandidateComponent } from './components/profile/profile-candidate/profile-candidate.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
