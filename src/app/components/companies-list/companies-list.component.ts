b43d13300b1f6fd627b6841951c0698923002523
Luciegaire <lucie83140@hotmail.fr>
2021-01-12 11:02:31 +0100

    style

--- src/app/components/companies-list/companies-list.component.ts
+++ src/app/components/companies-list/companies-list.component.ts
@@ -11,7 +11,26 @@ export class CompaniesListComponent implements OnInit {
     {
       id:0,
       name:"orange",
-      content:"entreprise de téléphonie"
+      content:"entreprise de téléphonie",
+      img:""
+    },
+    {
+      id:1,
+      name:"Mcdonald's",
+      content:"Restauration rapide",
+      img:""
+    },
+    {
+      id:2,
+      name:"Naval Group",
+      content:"entreprise pour l'armée",
+      img:""
+    },
+    {
+      id:3,
+      name:"Mcdonald's",
+      content:"Restauration rapide",
+      img:""
     }
   ]
   constructor() { }
