c9efa89fc0a528f36604f316e9fbba34f1162c1c
FlorentRicciardi <florent.ricciardi@sogeti.com>
2021-01-13 11:11:15 +0100

    Add back variable

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   toto() { return "ok" }
 }
