2a356a5710d379674f670d7b396d5ee13736ee25
antoineviggiano <52209696+antoineviggiano@users.noreply.github.com>
2021-01-13 12:10:34 +0100

    13/01 matin

--- src/app/components/profile/profile-company/profile-company.component.ts
+++ src/app/components/profile/profile-company/profile-company.component.ts
@@ -6,8 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-company.component.css']
 })
 export class ProfileCompanyComponent implements OnInit {
+  companyProfile = true;
+  companyOffers = false;
+  companyAccounts = false;
 
   constructor() { }
+
+  // tslint:disable-next-line: typedef
+  DisplayProfile() {
+    this.companyProfile = true;
+    this.companyOffers = false;
+    this.companyAccounts = false;
+  }
+
+  // tslint:disable-next-line: typedef
+  DisplayOffers() {
+    this.companyProfile = false;
+    this.companyOffers = true;
+    this.companyAccounts = false;
+  }
+
+  // tslint:disable-next-line: typedef
+  DisplayAccounts() {
+    this.companyProfile = false;
+    this.companyOffers = false;
+    this.companyAccounts = true;
+  }
 
   ngOnInit(): void {
   }
