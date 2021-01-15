fb579044d88b3063975135100400352e95f7de57
antoineviggiano <52209696+antoineviggiano@users.noreply.github.com>
2021-01-15 13:50:23 +0100

    15/01 matin

--- src/app/components/incom/incom-routing.module.ts
+++ src/app/components/incom/incom-routing.module.ts
@@ -5,6 +5,7 @@ import { CompaniesListComponent } from './companies-list/companies-list.componen
 import { IncomComponent } from './incom.component';
 import { OffersListComponent } from './offers-list/offers-list.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ProfileCompanyComponent } from './profile/profile-company/profile-company.component';
 
 const routes: Routes = [
   {
@@ -13,10 +14,11 @@ const routes: Routes = [
       { path: 'feed', component: FeedComponent, },
       { path: 'offers', component: OffersListComponent, },
       { path: 'profile', component: ProfileComponent, },
+      { path: 'profile-company', component: ProfileCompanyComponent, },
       { path: '**', redirectTo: 'feed', pathMatch: 'full' },
     ]
   },
-]
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
