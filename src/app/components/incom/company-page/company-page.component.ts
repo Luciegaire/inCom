9c6119cebff5ec0884f4a5251258380616fd7ab5
Luciegaire <lucie83140@hotmail.fr>
2021-02-20 18:39:58 +0100

    entreprise

--- src/app/components/incom/company-page/company-page.component.ts
+++ src/app/components/incom/company-page/company-page.component.ts
@@ -15,25 +15,97 @@ export class CompanyPageComponent implements OnInit {
   ) { }
 
   loading = true
+  sector =""
 
   id = ""
   company : {}
   offers : {}
+  contracts : []
+  companies : []
+  sectors : []
+
 
   getDate(date){
     return new Date(date)
   }
 
-  getCompany(id){
-    this.backendService.getCompanyById(id).subscribe({
-      next:(response) => {
-        this.company = response
-        console.log(this.company)
+
+  getCompany(id : number){
+    let array = this.companies.filter(x => x['company_id'] === id)
+    if(array.length != 0){
+      return array[0]['name']
+    }
+    else return ""
+  }
+
+  getContract(id : number){
+    let array = this.contracts.filter(x => x['contract_id'] === id)
+    if(array.length != 0){
+      return array[0]['name']
+    }
+    else return ""
+  }
+
+  getSector(id : number){
+    let array = this.sectors.filter(x => x['business_sector_id'] === id)
+    if(array.length != 0){
+      return array[0]['name']
+    }
+    else return ""
+  }
+
+  getBusinessSectors(){
+    this.backendService.getBusinessSectors().subscribe({
+      next: (response) => {
+        this.sectors = response
+      },
+      error: () =>{
+        console.log("erreur récupération secteurs")
+      },
+      complete: () =>{
+        this.loading = false
+      }
+    })
+  }
+
+  getCompanies(){
+    this.backendService.getCompanies().subscribe({
+      next: (response) => {
+        this.companies = response
+      },
+      error: () =>{
+        console.log("erreur récupération secteurs")
+      },
+      complete: () =>{
+      }
+    })
+  }
+
+  getContracts(){
+    this.backendService.getContracts().subscribe({
+      next: (response) => {
+        this.contracts = response
       },
       error: () => {
-        console.log("Erreur récup company")
+        console.log("Erreur création user")
       },
-      complete: () => {}
+      complete: () => {
+
+      }
+    })
+  }
+
+  getBusinessSector(id){
+    this.backendService.getSectorById(id).subscribe({
+      next: (response) => {
+        this.sector = response.name
+      },
+      error : () => {
+        console.log("erreur récup ")
+      },
+      complete: () => {
+
+      }
     })
   }
 
@@ -42,6 +114,20 @@ export class CompanyPageComponent implements OnInit {
       next : (response) =>{
         console.log("offers", response)
         this.offers = response
+      },
+      error: () => {
+        console.log("Error retrieving posts")
+      },
+      complete : () => {
+      }
+    })
+  }
+
+  getCompanyById(id){
+    this.backendService.getCompanyById(id).subscribe({
+      next : (response) =>{
+        console.log("company", response)
+        this.company = response
       },
       error: () => {
         console.log("Error retrieving posts")
@@ -57,7 +143,10 @@ export class CompanyPageComponent implements OnInit {
       if (routeParams) {
         console.log("paramètres de la route", routeParams)
         this.id = routeParams.id
-        this.getCompany(this.id)
+        this.getCompanyById(parseInt(this.id))
+        this.getBusinessSectors()
+        this.getContracts()
+        this.getCompanies()
         this.getOffers(this.id)
       }
     });
